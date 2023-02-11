54f3fee08189734073c236824f4ceead1264e6c4
Duminicstefan <aspxx1992@gmail.com>
2023-02-11 14:16:49 +0200

    acasa

---
+++ Exercitii/exercitii.js
@@ -0,0 +1,35 @@
+// const romania = 33;
+// const portugalia = 22;
+
+// if (romania > 33){
+//     console.log(`${romania}'pouplation is above average`);
+// }
+// else{console.log(`${portugalia}population is ${33 - portugalia}milion below average`);}
+
+
+// const numNeibghors = 0;
+// prompt("cate tari vecine are tara ta");
+// if(numNeibghors == 1){console.log("tara ta are mai multi venini")}
+// else if (numNeibghors !=2 ){console.log('mai mult de o granita')}
+// else{console.log("fara granmite")}
+// const country = "romania";
+// const language = "english";
+// const population =30;
+// const island =false;
+
+// if(language === "english" && population < 50 && !island){
+//     console.log( `Se poate muta in ${country}`)
+// }
+// else{console.log(`Nu se poate muta ${country}`)}
+
+
+const team1 = "Delfinii";
+const team2 = "Koalas";
+ 
+const scor1 =(97 + 112 +101 )/3;
+const scor2 = (109 + 95 + 123)/3;
+const scor3=100;
+
+if(scor1==scor2 ){console.log(`${team1} castiga`)}
+else if(scor1,scor2>scor3)
+console.log() 

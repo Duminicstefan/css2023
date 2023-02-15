809770386feb9b9679bfd65fc88d5707f2a13dd1
Duminicstefan <aspxx1992@gmail.com>
2023-02-15 21:47:05 +0200

    acasa

---
+++ Exercitii/exercitii1.js
@@ -0,0 +1,96 @@
+// VALORI SI VARIABILE !!
+// let js = "amazing";
+
+// let myName="numele meu este Ion";
+// let age = 23;
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+
+
+
+
+//  VAlORI SI VARIABILE CHALANGE!!!!
+
+// const country = "Romania";
+// const continent ="Europe";
+// let population = 18;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// TIPURI DE DATE!!!!
+
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// javaScriptIsFun = false;
+// console.log(javaScriptIsFun);
+//  console.log(typeof javaScriptIsFun);
+//  javaScriptIsFun=23;
+//  console.log(typeof javaScriptIsFun);
+//  javaScriptIsFun ="Ion";
+//  console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year=1991;
+// console.log(typeof year);
+
+// TIPURI DE DATE CHALANGE!!!
+
+// let island = "Romania";
+// let language;
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof island);
+// console.log(typeof language);
+
+
+// OPERATORII DE BAZA!!!!
+
+// let now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// // Operatorii matematici!
+// console.log(ageJonas);
+// console.log(ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)// 2 ** 3 = ionseamna baza 2 la putetrea 3, 2 * 2 * 2 =8
+
+// const firstName = "Ion";
+// const lastName = "Ciobanu"
+// console.log(firstName + lastName);
+
+// // Assigment operators
+
+// let x = 10 + 5 //15
+// x += 10; // x = x +10
+// x -= 5; //20
+// x *= 5; //100
+// console.log(x);
+
+// x ++; //x = x + 1
+// x--;// x= x - 1
+
+// // Comparison operators
+//  console.log (ageJonas >= ageSarah);
+//  console.log(ageSarah  >= 18);
+ 
+//  const itsFullAge = ageSarah>=18
+//  console.log(itsFullAge);
+//  console.log( now -1991 >now -2021);
+
+
+//  OPERATORII DE BAZA CHALANGE !!!!
+
+console.log(population / 2);
+population ++;
+console.log(population);
+console.log(poulation > 6);
+console.log(population < 33);
+const description = country + 'is in' + continent +',and it-s' population + 'milion people speak'+language;
+console.log(description);
